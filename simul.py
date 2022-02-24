4609613e3cc3eb369f6b20ebf3a038411ede2e26
Parikshit Chalise <chaliseparik@macbook-pro.soka.edu>
2022-02-23 16:59:46 -0800

    created coordinates list

--- simul.py
+++ simul.py
@@ -19,13 +19,21 @@ def main_list(m,n):
 	#selecting one of the coordinates to start the game
 
 	random_index = random.randint(0,len(node_list)-1)
-	#print(node_list[random_index])
+	first_node = (node_list[random_index])
+	#print(first_node)
 
 	#creating the neighbor list of the selected coordinate
+	x = first_node[0]
+	y = first_node[1]
+	#print(x,y)
 
+	neighbor_list =[[x,y],[x-1,y],[x,y-1], [x+1,y], [x,y+1]]
+	#print(neighbor_list)	
 	
-
-	
+	#need to code for nuances in neighbors. corners have only two neighbors. sides have only two.
+	#as the game progresses, neighbors can only be from the list not already in selected	
 	
 	
 main_list(5,3)
+
+
